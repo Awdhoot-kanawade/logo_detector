f10cda0c0eb642fe9ad8686e61b4ebe5d09501db
Awdhoot Kanawade <awdhootkanawade@gmail.com>
2023-07-27 19:04:56 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -31,7 +31,7 @@ case ${target} in
    cpu)  
       echo "Installing icevision + dependencices for ${1}"
       echo "- Installing torch and its dependencies"
-      pip install torch=="1.10.0+cpu" torchvision=="0.11.1+cpu" torchtext==0.11.0 -f https://download.pytorch.org/whl/torch_stable.html  
+      pip install torch=="1.11.0+cpu" torchvision=="0.11.1+cpu" torchtext==0.11.0 -f https://download.pytorch.org/whl/torch_stable.html  
 
       echo "- Installing mmcv"
       pip install mmcv-full=="1.3.17" -f https://download.openmmlab.com/mmcv/dist/cpu/torch1.10.0/index.html --upgrade -q  
